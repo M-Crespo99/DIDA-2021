b6d4d483149c569bcc43ac0ebb15eb36285fb0d8
Wallace Garbim <wallace.garbim@tui.pt>
2021-10-11 15:49:04 +0100

    Added GRPC, GRPC Tools, Google proto buffer dependencies and the main class to start the PCS server.

--- PCS/Program.cs
+++ PCS/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using Grpc.Core;
 
 namespace PCS
 {
     class Program
     {
+        private const int Port = 10000;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Ports = {new ServerPort("localhost", Port, ServerCredentials.Insecure)}
+                };
+                server.Start();
+                Console.WriteLine("The PCS server is listening on the port: " + Port);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
         }
     }
 }
