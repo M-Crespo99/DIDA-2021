c8987971508868cf45c397209f5c82a1559ddf3d
Wallace Garbim <wallace.garbim@tui.pt>
2021-10-14 17:47:11 +0100

    Added logic to store the worker and storage infos and added the worker as server GRPC using port as VM args

--- PCS/PCSServiceImpl.cs
+++ PCS/PCSServiceImpl.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Core;
 using static PCSService;
@@ -7,11 +9,20 @@ namespace PCS
 {
     public class PCSServiceImpl : PCSServiceBase
     {
+        private static readonly int NumProcs = Environment.ProcessorCount;
+        private static readonly int ConcurrencyLevel = NumProcs * 2;
+        private static int Counter = 5000;
+        
+        private ConcurrentDictionary<int, string> _portWorker = new (ConcurrencyLevel, 100);
+        private ConcurrentDictionary<int, string> _portStorage = new (ConcurrencyLevel, 100);
         public override async Task<PCSRunWorkerReply> runWorker(PCSRunWorkerRequest request, ServerCallContext context)
         {
             Console.WriteLine("## Testing parameters for Run Worker ##");
             Console.WriteLine(request.ToString());
             Console.WriteLine("## ------ ##");
+
+            var counter = Interlocked.Increment(ref Counter);
+            _portWorker.TryAdd(counter, String.Format("Worker-{0}", counter));
 
             return await Task.FromResult(new PCSRunWorkerReply {Ok = true});
         }
@@ -21,6 +32,10 @@ namespace PCS
             Console.WriteLine("## Testing parameters for Run Storage ##");
             Console.WriteLine(request.ToString());
             Console.WriteLine("## ------ ##");
+            
+            var counter = Interlocked.Increment(ref Counter);
+            _portStorage.TryAdd(counter, String.Format("Storage-{0}", counter));
+            
             return await Task.FromResult(new PCSRunStorageReply {Ok = true});
         }
     }
--- worker/Program.cs
+++ worker/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Grpc.Core;
+using Grpc.Reflection;
+using Grpc.Reflection.V1Alpha;
+using static Grpc.Core.ServerCredentials;
 
 namespace worker
 {
@@ -6,7 +10,31 @@ namespace worker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server server = null;
+
+            try
+            {
+                var port = int.Parse(args[0]);
+                server = new Server
+                {
+                    Ports = {new ServerPort("localhost", port, Insecure)}
+                };
+                server.Start();
+                Console.WriteLine("The Worker server is listening on the port: " + port);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
         }
     }
 }
