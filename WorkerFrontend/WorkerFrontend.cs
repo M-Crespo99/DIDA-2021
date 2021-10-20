ce97236cabebe3fef0f20b48435d860e67b7f20c
M.Crespo <miguelcampelocrespo@gmail.com>
2021-10-20 17:21:16 +0100

    First implementation of Scheduler

---
+++ WorkerFrontend/WorkerFrontend.cs
@@ -0,0 +1,58 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+
+namespace WorkerFrontend
+{
+    public class Frontend
+    {
+        private int _port;
+
+        private string _host;
+        private GrpcChannel _channel;
+        private DIDAWorker.Proto.DIDAWorkerService.DIDAWorkerServiceClient _client;
+
+        private string _lastErrorMessage = "";
+
+        private bool _verbose = false;
+
+        public Frontend(string host, int port)
+        {
+            this._port = port;
+            this._host = host;
+
+            this._verbose = false;
+
+            this._channel = GrpcChannel.ForAddress("http://" + host + ":" + port);
+
+            this._client = new DIDAWorker.Proto.DIDAWorkerService.DIDAWorkerServiceClient(this._channel);
+        }
+
+        public Frontend(string host, int port, bool verbose)
+        {
+            this._port = port;
+            this._host = host;
+
+            this._verbose = verbose;
+
+            this._channel = GrpcChannel.ForAddress("http://" + host + ":" + port);
+
+            this._client = new DIDAWorker.Proto.DIDAWorkerService.DIDAWorkerServiceClient(this._channel);
+        }
+
+        public void workOnOperator(DIDAWorker.Proto.DIDARequest request){
+            this._client.workOnOperatorAsync(request); //Do we care about this return value?
+        }
+
+        public string getLastErrorMessage(){
+            return this._lastErrorMessage;
+        }
+
+        public void crashServer(){
+            throw new NotImplementedException();
+        }
+        public void ToggleVerbose(){
+            this._verbose = !this._verbose;
+        }
+    }
+}
--- scheduler/SchedulerServiceImpl.cs
+++ scheduler/SchedulerServiceImpl.cs
@@ -1,17 +1,54 @@
 using System;
 using System.Threading.Tasks;
 using Grpc.Core;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
 using static DIDASchedulerService;
+using System.Linq;
 
 namespace scheduler
 {
+
+
     public class SchedulerServiceImpl : DIDASchedulerServiceBase
     {
+
+        private List<string> _storages = new List<string>();
+        private List<string> _workers = new List<string>();
+
+        private bool _verbose = true;
+
+        private int _currentWorkerOrder = 0;
+
+        private int _idCounter = 0;
         public override async Task<DIDARunApplicationReply> runApplication(DIDARunApplicationRequest request, ServerCallContext context)
         {
-            Console.WriteLine("## Testing parameters for Run application ##");
-            Console.WriteLine(request.ToString());
-            Console.WriteLine("## ------ ##");
+
+            this.ParseServers(this._workers, request.Workers.ToList());
+            this.ParseServers(this._workers, request.Storages.ToList());
+
+            //Get the operators
+            var operators = this.ReadApplicationFile(request.FilePath);
+
+            //sort them
+            operators = operators.OrderBy(op => op.Item2).ToList();
+
+            
+            DIDAWorker.Proto.DIDARequest newRequest = new DIDAWorker.Proto.DIDARequest();
+
+            newRequest.Meta.Id = _idCounter++;
+            newRequest.Input = request.Input;
+            newRequest.Next = 0;
+            newRequest.ChainSize = operators.Count;
+
+            this.ScheduleOperators(newRequest, operators);
+            this.AssignStorageDetails(newRequest);
+
+
+            WorkerFrontend.Frontend workerFrontend = new WorkerFrontend.Frontend(newRequest.Chain.First().Host, newRequest.Chain.First().Port);
+
+            workerFrontend.workOnOperator(newRequest);            
+            
             return await Task.FromResult(new DIDARunApplicationReply{Ok = true});
         }
 
@@ -30,5 +67,80 @@ namespace scheduler
             Console.WriteLine("## ------ ##");
             return await Task.FromResult(new LivenessCheckReply{Ok = true});
         }
+
+
+        private List<Tuple<string, int>> ReadApplicationFile(string filePath){
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            var listToReturn = new List<Tuple<string, int>>();
+            foreach(string line in lines){
+                var parts = line.Split(" ");
+
+                if(parts[0] == "operator" && parts.Length == 3){
+                    var currentTuple = new Tuple<string, int>(parts[1], Int32.Parse(parts[2]));
+                    listToReturn.Add(currentTuple);
+                }
+            }
+            return listToReturn;
+        }
+
+        private void ParseServers(List<string> listToAdd ,List<string> nodes){
+            foreach(string node in nodes){
+                if(!listToAdd.Contains(node)){
+                    listToAdd.Add(node);
+                }
+                else{
+                    if(this._verbose){
+                        Console.WriteLine("WARNING: could not parse node: {0}", node);
+                    }
+                }
+            }
+        }
+
+        private Tuple<string, string> GetAddressInfo(string URL){
+            var parts = URL.Split(":");
+            if(parts.Length == 2){
+                return new Tuple<string, string>(parts[0], parts[1]);
+            }
+            return null;
+        }
+
+        private void ScheduleOperators(DIDAWorker.Proto.DIDARequest request, List<Tuple<string, int>> operators){
+            //Round robin implementation of load distribution
+
+            if(this._workers.Count == 0){
+                //TODO; Do something?
+            }
+
+            foreach(Tuple<string, int> op in operators){
+                var hostInfo = this.GetAddressInfo(this._workers[this._currentWorkerOrder]);
+
+                var assignment = new DIDAWorker.Proto.DIDAAssignment{
+                    Operator = new DIDAWorker.Proto.DIDAOperatorID{
+                        Classname = op.Item1,
+                        Order = op.Item2,
+                    },
+                    Host = hostInfo.Item1,
+                    Port = Int32.Parse(hostInfo.Item2),
+                    Output = ""
+                };
+                request.Chain.Add(assignment);
+                this._currentWorkerOrder = this._currentWorkerOrder % this._workers.Count;
+            }
+        }
+
+        private void AssignStorageDetails(DIDAWorker.Proto.DIDARequest request){
+            foreach(string storage in this._storages){
+                var storageInfo = this.GetAddressInfo(storage);
+                request.Meta.Storages.Add(
+                    new DIDAWorker.Proto.DIDAStorageNodeDetails{
+                        Host = storageInfo.Item1,
+                        Port = Int32.Parse(storageInfo.Item2),
+                    }
+                );
+            }
+        }
+        
     }
+    
+
 }
