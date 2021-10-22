44487f2face1b875af95fac232c7c942da3767d0
M.Crespo <miguelcampelocrespo@gmail.com>
2021-10-22 17:48:51 +0100

    added dida writer

--- run_script.sh
+++ run_script.sh
@@ -1,3 +1,8 @@
+# fullname="USER INPUT"
+read -p "Enter app you want to run: " app
+# user="USER INPUT"
+read -p "Enter Input for the app: " input
 
 
-~/Downloads/grpcurl --plaintext -d '{"input": "123456", "filePath": "app2", "workers": [ "localhost:5002", "localhost:5003"], "storages": [ "localhost:5001" ], "schedulerUrl": "localhost:5004"}' localhost:2000 PuppetMasterService/runApplication
+
+~/Downloads/grpcurl --plaintext -d '{"input": "'${input}'", "filePath": "'${app}'", "workers": [ "localhost:5002", "localhost:5003"], "storages": [ "localhost:5001" ], "schedulerUrl": "localhost:5004"}' localhost:2000 PuppetMasterService/runApplication
